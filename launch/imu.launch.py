1071f3858a6e7f538280ac03fa9d7ecda6c7ecbc
Mariolopez31 <al427817@uji.es>
2026-05-29 12:37:47 +0100

    integration

--- launch/imu.launch.py
+++ launch/imu.launch.py
@@ -3,7 +3,6 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
@@ -17,66 +16,51 @@ def generate_launch_description():
 
     raw_imu_topic = LaunchConfiguration("raw_imu_topic")
     mag_topic = LaunchConfiguration("mag_topic")
-    output_imu_topic = LaunchConfiguration("output_imu_topic")
     calibrated_imu_topic = LaunchConfiguration("calibrated_imu_topic")
     calibrated_mag_topic = LaunchConfiguration("calibrated_mag_topic")
-    base_frame = LaunchConfiguration("base_frame")
-    imu_frame = LaunchConfiguration("imu_frame")
+    filtered_imu_topic = LaunchConfiguration("filtered_imu_topic")
     fixed_frame = LaunchConfiguration("fixed_frame")
     world_frame = LaunchConfiguration("world_frame")
     use_mag = LaunchConfiguration("use_mag")
     use_calibration = LaunchConfiguration("use_calibration")
     calibration_yaml = LaunchConfiguration("calibration_yaml")
     publish_filter_tf = LaunchConfiguration("publish_filter_tf")
-    publish_static_tf = LaunchConfiguration("publish_static_tf")
     gain = LaunchConfiguration("gain")
     zeta = LaunchConfiguration("zeta")
     orientation_stddev = LaunchConfiguration("orientation_stddev")
     constant_dt = LaunchConfiguration("constant_dt")
-    imu_x = LaunchConfiguration("imu_x")
-    imu_y = LaunchConfiguration("imu_y")
-    imu_z = LaunchConfiguration("imu_z")
-    imu_roll = LaunchConfiguration("imu_roll")
-    imu_pitch = LaunchConfiguration("imu_pitch")
-    imu_yaw = LaunchConfiguration("imu_yaw")
 
     return LaunchDescription(
         [
             DeclareLaunchArgument(
                 "raw_imu_topic",
-                default_value="/imu_broadcaster/imu",
+                default_value="controller/imu_broadcaster/imu",
             ),
             DeclareLaunchArgument(
                 "mag_topic",
-                default_value="/magnetometer_broadcaster/mag",
+                default_value="magnetometer_broadcaster/mag",
             ),
-            DeclareLaunchArgument("output_imu_topic", default_value="/sura/imu/data"),
             DeclareLaunchArgument(
                 "calibrated_imu_topic",
-                default_value="/sura/imu/data_raw_calibrated",
+                default_value="imu/data_raw",
             ),
             DeclareLaunchArgument(
                 "calibrated_mag_topic",
-                default_value="/sura/imu/mag_calibrated",
+                default_value="imu/mag",
             ),
-            DeclareLaunchArgument("base_frame", default_value="bluerov2/base_link"),
-            DeclareLaunchArgument("imu_frame", default_value="bluerov2/imu_link"),
+            DeclareLaunchArgument(
+                "filtered_imu_topic",
+                default_value="sensors/imu",
+            ),
             DeclareLaunchArgument("fixed_frame", default_value="world_ned"),
             DeclareLaunchArgument("world_frame", default_value="ned"),
             DeclareLaunchArgument("use_mag", default_value="true"),
             DeclareLaunchArgument("use_calibration", default_value="true"),
             DeclareLaunchArgument("calibration_yaml", default_value=calibration_file),
             DeclareLaunchArgument("publish_filter_tf", default_value="false"),
-            DeclareLaunchArgument("publish_static_tf", default_value="false"),
             DeclareLaunchArgument("gain", default_value="0.2"),
             DeclareLaunchArgument("zeta", default_value="0.0"),
             DeclareLaunchArgument("orientation_stddev", default_value="0.02"),
-            DeclareLaunchArgument("imu_x", default_value="0.0"),
-            DeclareLaunchArgument("imu_y", default_value="0.0"),
-            DeclareLaunchArgument("imu_z", default_value="0.0"),
-            DeclareLaunchArgument("imu_roll", default_value="0.0"),
-            DeclareLaunchArgument("imu_pitch", default_value="0.0"),
-            DeclareLaunchArgument("imu_yaw", default_value="0.0"),
             DeclareLaunchArgument("constant_dt", default_value="0.0"),
             Node(
                 package="sura_imu",
@@ -115,7 +99,7 @@ def generate_launch_description():
                 remappings=[
                     ("imu/data_raw", calibrated_imu_topic),
                     ("imu/mag", calibrated_mag_topic),
-                    ("imu/data", output_imu_topic),
+                    ("imu/data", filtered_imu_topic),
                 ],
             ),
         ]
